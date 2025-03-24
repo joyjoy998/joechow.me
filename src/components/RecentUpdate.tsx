8a1431bc7943a1b9748e1a5add6cb6b130185daf
Haochuan Zhou <mrzhouhc@gmail.com>
2025-03-24 11:45:19 +1100

    Recent blogs done

--- src/components/RecentUpdate.tsx
+++ src/components/RecentUpdate.tsx
@@ -1,15 +1,8 @@
 import { Newspaper } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-
-interface Blog {
-  slug: string;
-  title: string;
-  image: string;
-  publishedAt?: string;
-  tag: string;
-  summary: string;
-}
+import { Blog } from "@/types/blogType";
+import { timeFormatConverter } from "@/lib/timeFormatConverter";
 
 type RecentUpdateProps = {
   blogs: Blog[];
@@ -24,26 +17,28 @@ export default function RecentUpdate({ blogs }: RecentUpdateProps) {
       </div>
       <ul className="grid w-full grid-cols-1 gap-10">
         {blogs.map((blog) => (
-          <li key={blog.slug}>
-            <Link href={`/blog/${blog.slug}`}>
-              <div className="relative rounded-2xl hover:shadow-[0_0px_2px_rgb(140,140,140)] shadow-[0_0px_1.2px_rgb(140,140,140)] opacity-70 hover:opacity-90">
-                <div className="relative aspect-240/135 w-full ">
+          <li key={blog.slug.current}>
+            <Link href={`/blog/${blog.slug.current}`}>
+              <div className="relative overflow-hidden rounded-3xl hover:shadow-[0_0px_2px_rgb(140,140,140)] shadow-[0_0px_1.2px_rgb(140,140,140)] opacity-70 hover:opacity-90">
+                <div className="relative aspect-[240/135] w-full ">
                   <Image
-                    src={blog.image}
+                    src={blog.imageUrl}
                     alt="Blog image"
                     fill
-                    className="object-contain rounded-2xl "
+                    className="object-cover"
                   />
                 </div>
 
-                <div className="absolute bottom-0 left-0 right-0 p-4 rounded-lg backdrop-blur-3xl">
-                  <h2 className="mb-2 font-bold">{blog.title}</h2>
-                  <p className="mb-4 text-sm text-muted-foreground">
-                    {blog.publishedAt ?? ""} | {blog.tag}
-                  </p>
-                  <p className="text-sm text-transparent bg-linear-to-l from-muted-foreground via-foreground to-muted-foreground bg-clip-text">
-                    {blog.summary}
-                  </p>
+                <div className="absolute bottom-0 left-0 right-0 py-2 px-4 rounded-b-3xl backdrop-blur-3xl">
+                  <h2 className="font-bold text-sm md:text-base text-foreground">
+                    {blog.title}
+                  </h2>
+                  <div className="flex justify-between items-center mt-1">
+                    <p className="text-xs text-muted-foreground">
+                      {timeFormatConverter(blog.publishedAt)}{" "}
+                      {blog.tag ? `| ${blog.tag}` : ""}
+                    </p>
+                  </div>
                 </div>
               </div>
             </Link>
