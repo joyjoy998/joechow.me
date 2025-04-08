858e3b6d0ea5de5ca08e7b9e7f62bccfa0d4166c
Haochuan Zhou <mrzhouhc@gmail.com>
2025-04-08 23:09:04 +1000

    the same as previous commit

--- src/app/(root)/project/page.tsx
+++ src/app/(root)/project/page.tsx
@@ -3,6 +3,8 @@ import Description from "@/components/Description";
 import Projects from "@/components/Projects";
 import apiClient from "@/lib/apiClient";
 
+export const dynamic = "force-dynamic";
+
 export default async function Page() {
   const res = await apiClient.get("/api/project/get");
   const { data, success } = res.data;
