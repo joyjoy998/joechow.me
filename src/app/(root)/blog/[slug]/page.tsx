b0c6b5ccb15309c05c409f73839c1ddd2f094958
Haochuan Zhou <mrzhouhc@gmail.com>
2025-04-08 13:17:27 +1000

    Fix again

--- src/app/(root)/blog/[slug]/page.tsx
+++ src/app/(root)/blog/[slug]/page.tsx
@@ -6,13 +6,12 @@ import { formatDistanceToNow } from "date-fns";
 import apiClient from "@/lib/apiClient";
 import { notFound } from "next/navigation";
 
-type Props = {
+type Params = Promise<{
   params: { slug: string };
-  searchParams?: { [key: string]: string | string[] | undefined };
-};
+}>;
 
-export default async function Blog({ params }: Props) {
-  const slug = params.slug;
+export default async function Blog({ params }: { params: Params }) {
+  const slug = await params;
   const res = await apiClient.get(`/api/blog/get/${slug}`);
   const { data, success } = res.data;
   // console.log(success);
